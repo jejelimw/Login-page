5523d222fe0df0cab5575657b8db4fab952b4d77
jess <jessicamosby12@gmail.com>
2024-05-20 10:30:39 -0300

    restruturando layout

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { FacebookLoginProvider } from '@abacritt/angularx-social-login';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -11,7 +13,6 @@ export class DefaultLoginLayoutComponent {
   @Input() title: string = '';
   @Input() primaryBtnText = '';
   @Input() secondaryBtnText = '';
-  @Input() others = '';
   @Input() control?: FormGroup | undefined;
   @Input() loginValid: boolean = false;
   @Input() signValid: boolean = false;
@@ -19,6 +20,9 @@ export class DefaultLoginLayoutComponent {
   @Output('submit') onSubmit = new EventEmitter();
 
   loginError = false;
+
+  user: SocialUser | undefined;
+  loggedIn: boolean = false;
 
   constructor(public router: Router) {}
 
