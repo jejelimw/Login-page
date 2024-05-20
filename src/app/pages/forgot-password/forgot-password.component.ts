a5fd92f5ee9033c3e27c029fa1403752bf89388b
jess <jessicamosby12@gmail.com>
2024-05-20 10:29:57 -0300

    config

---
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password',
+
+  templateUrl: './forgot-password.component.html',
+  styleUrl: './forgot-password.component.scss',
+})
+export class ForgotPasswordComponent {}
